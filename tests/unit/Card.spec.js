0a4d67bb19d34100ca6769871e183060fe2abad2
Victor Borràs <vborrasc@gmail.com>
2021-12-21 18:02:35 +0100

    add html slots test

--- tests/unit/Card.spec.js
+++ tests/unit/Card.spec.js
@@ -13,4 +13,15 @@ describe('Card.vue', () => {
     expect(wrapper.html()).to.include('Hello')
     expect(wrapper.html()).to.include('World')
   })
+
+  it('renders HTML slots', () => {
+    const wrapper = mount(Card, {
+      slots: {
+        header: '<strong>Hello</strong>',
+        body: '<a href="#">World</a>'
+      }
+    })
+    expect(wrapper.html()).to.include('<strong>Hello</strong>')
+    expect(wrapper.html()).to.include('<a href="#">World</a>')
+  })
 })
